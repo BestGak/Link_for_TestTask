1ccfaba313a031522652a3e7a69377b21aeadf10
BestGak <79905081+BestGak@users.noreply.github.com>
2021-07-22 12:32:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '.splide', {
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
         pagination: false,
         // autoplay: 'playing',
         // rewind: true,
         padding: {
-            left : 0,
-            right: '6rem',
+            left : '2rem',
+            right: '2rem',
         }, 
         gap: '20px',
-        width: '916px' 
-    }).mount();
-} );
+        // width: '1115px',
+        // fixedWidth: '820px',
+        autoWidth: true,
+        breakpoints: {
+            1024: {
+                pagination: true,
+                arrows: false,
+                fixedWidth: '100%'  ,
+                gap: '120px'
+            }
+        }      
+    }).mount();    
+});
